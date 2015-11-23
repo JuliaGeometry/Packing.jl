47af1eebe9f6021330be2be6cf2baf19080ff1bc
Steve Kelly <kd2cca@gmail.com>
2015-11-22 22:56:08 -0500

    fix Rectangle -> SimpleRectangle deprecation

--- src/rectangle.jl
+++ src/rectangle.jl
@@ -6,23 +6,23 @@ type BinaryNode{T}
 end
 type RectanglePacker{T}
     children::BinaryNode{RectanglePacker{T}}
-    area::Rectangle{T}
+    area::SimpleRectangle{T}
 end
 
 left(a::RectanglePacker)                                = get(a.children.left)
 left{T}(a::RectanglePacker{T}, r::RectanglePacker{T})   = (a.children.left = Nullable(r))
 right(a::RectanglePacker)                               = get(a.children.right)
 right{T}(a::RectanglePacker{T}, r::RectanglePacker{T})  = (a.children.right = Nullable(r))
-RectanglePacker{T}(area::Rectangle{T})                  = RectanglePacker{T}(BinaryNode{RectanglePacker{T}}(), area)
+RectanglePacker{T}(area::SimpleRectangle{T})            = RectanglePacker{T}(BinaryNode{RectanglePacker{T}}(), area)
 isleaf(a::RectanglePacker)                              = isnull(a.children.left) && isnull(a.children.right)
 
 # This is rather append, but it seems odd to use another function here.
 # Maybe its a bad idea, to call it push regardless!?
-function Base.push!{T}(node::RectanglePacker{T}, areas::Vector{Rectangle{T}})
+function Base.push!{T}(node::RectanglePacker{T}, areas::Vector{SimpleRectangle{T}})
     sort!(areas)
     RectanglePacker{T}[push!(node, area) for area in areas]
 end
-function Base.push!{T}(node::RectanglePacker{T}, area::Rectangle{T})
+function Base.push!{T}(node::RectanglePacker{T}, area::SimpleRectangle{T})
     if !isleaf(node)
         l = push!(left(node), area)
         l == nothing && return push!(right(node), area) # if left does not have space, try right
@@ -33,9 +33,9 @@ function Base.push!{T}(node::RectanglePacker{T}, area::Rectangle{T})
         oax,oay,oaxw,oayh   = node.area.x+newarea.w, node.area.y, xwidth(node.area), node.area.y + newarea.h
         nax,nay,naxw,nayh   = node.area.x, node.area.y+newarea.h, xwidth(node.area), yheight(node.area)
         rax,ray,raxw,rayh   = node.area.x, node.area.y, node.area.x+newarea.w, node.area.y+newarea.h
-        left(node,  RectanglePacker(Rectangle(oax, oay, oaxw-oax, oayh-oay)))
-        right(node, RectanglePacker(Rectangle(nax, nay, naxw-nax, nayh-nay)))
-        return RectanglePacker(Rectangle(rax,ray,raxw-rax,rayh-ray))
+        left(node,  RectanglePacker(SimpleRectangle(oax, oay, oaxw-oax, oayh-oay)))
+        right(node, RectanglePacker(SimpleRectangle(nax, nay, naxw-nax, nayh-nay)))
+        return RectanglePacker(SimpleRectangle(rax,ray,raxw-rax,rayh-ray))
     end
 end
 
