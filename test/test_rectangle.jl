287fcb62aab11283b68d8696af1150aa7e4057db
Jeremie Knuesel <knuesel@gmail.com>
2021-07-20 09:33:09 +0200

    Replace IRect2D with Rect2i, etc.

--- test/test_rectangle.jl
+++ test/test_rectangle.jl
@@ -1,10 +1,10 @@
-root = RectanglePacker(Rect2D(0,0,1024,1024))
-push!(root, Rect2D(0,0,20,20))
-push!(root, Rect2D(0,0,20,20))
-push!(root, [Rect2D(0,0, rand(5:50), rand(5:50)) for i=1:20])
+root = RectanglePacker(Rect2(0,0,1024,1024))
+push!(root, Rect2(0,0,20,20))
+push!(root, Rect2(0,0,20,20))
+push!(root, [Rect2(0,0, rand(5:50), rand(5:50)) for i=1:20])
 
 
-# function get_rectangles(packer::Nothing, rectangles=IRect2D[])
+# function get_rectangles(packer::Nothing, rectangles=Rect2i[])
 #     return rectangles
 # end
 #
@@ -15,7 +15,7 @@ push!(root, [Rect2D(0,0, rand(5:50), rand(5:50)) for i=1:20])
 #     return rectangles
 # end
 # function get_rectangles(packer)
-#     rectangles = IRect2D[]
+#     rectangles = Rect2i[]
 #     get_rectangles(packer.children.left, rectangles)
 #     get_rectangles(packer.children.right, rectangles)
 #     return rectangles
@@ -23,6 +23,6 @@ push!(root, [Rect2D(0,0, rand(5:50), rand(5:50)) for i=1:20])
 #
 # rectangles = get_rectangles(root)
 # using Makie
-# linesegments(FRect(0, 0, 1024, 1024))
+# linesegments(Rectf(0, 0, 1024, 1024))
 # poly!(rectangles, color = (:red, 0.1), strokewidth=1, strokecolor=:black)
 #
